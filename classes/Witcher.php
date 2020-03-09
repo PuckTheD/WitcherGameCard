5ca44fea44598a38dd9eecbae14d3214cf5f1f63
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-03-09 20:07:59 +0100

    update class Witcher

--- classes/Witcher.php
+++ classes/Witcher.php
@@ -5,12 +5,7 @@
  */
 class Witcher extends TemplatePerso
 {
-
-	public function __construct($n, $pv, $str, $def, $sta, $exp)
-	{
-		$this->name = $n;
-		$this->experience = 0;
-	}
+	public int signe;
 }
 
  ?>
