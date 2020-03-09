feb6cd4b4ae3ca0b4ddba8a2234993807a4046ee
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-03-09 19:15:15 +0100

    Add project

---
+++ classes/TemplatePerso.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * summary
+ */
+class TemplatePerso
+{
+
+	public string $name;
+	public int $lifePoint;
+	public int $strong;
+	public int $defence;
+	public int $stamina;
+	public int $experience
+
+    public function physicAttack($perso)
+    {
+    	
+    }
+}
+
+ ?>
---
+++ classes/Witcher.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * summary
+ */
+class Witcher extends TemplatePerso
+{
+
+	public function __construct($n, $pv, $str, $def, $sta, $exp)
+	{
+		$this->name = $n;
+		$this->experience = 0;
+	}
+}
+
+ ?>
