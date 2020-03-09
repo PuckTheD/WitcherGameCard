c40338a5b21c225f698fd9cfed3d54ed40cece51
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-03-09 20:05:24 +0100

    update class monster

---
+++ classes/Monster.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * summary
+ */
+class Monster extends TemplatePerso
+{
+    /**
+     * summary
+     */
+    public function __construct($name, $pv, $str, $def, $spr)
+    {
+        
+    }
+}
+
+ ?>
