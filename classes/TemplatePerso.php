184932dd1c8480f041ad6a5acb73db6f4a834fbb
Alexandre HOSSEIN <alex.hos.27@gmail.com>
2020-03-09 20:07:11 +0100

    update class TemplatePerso
    add function physicAttack
    
    update attribute

--- classes/TemplatePerso.php
+++ classes/TemplatePerso.php
@@ -10,12 +10,19 @@ class TemplatePerso
 	public int $lifePoint;
 	public int $strong;
 	public int $defence;
+	public int $spirit;
 	public int $stamina;
 	public int $experience
+	public bool $alive = true;
+
+	if ($this->lifePoint <= 0) {
+		$this->alive = false;
+	}
 
     public function physicAttack($perso)
     {
-    	
+    	$perso->lifePoint -= $this->strong - $perso->defence * rand(0, $perso->defence - 1);
+    	$this->experience + rand(2, 5);
     }
 }
 
